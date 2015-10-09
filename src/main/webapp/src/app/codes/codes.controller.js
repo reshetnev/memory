6f0cd938d6a23b9cb932598c695050c954aa226d
Aleksandr Reshetnev <a.v.reshetnev@gmail.com>
2015-10-09 18:48:21 +0300

    Updated Error REST to test data service

--- src/main/webapp/src/app/codes/codes.controller.js
+++ src/main/webapp/src/app/codes/codes.controller.js
@@ -10,15 +10,13 @@ function codesCtrl($http, Data) {
     /* jshint validthis: true */
     var vm = this;
 
-//    $http({method:'GET', url:'http://localhost:7001/memory/api/v1/codes'})
-//        .success(function (data, status, headers, config) {
-//            vm.codesList = data;
-//        })
-//        .error(function (data, status, headers, config) {
-//            alert('Error REST');
-//        });
-
-    vm.codesList = Data.getCodes();
+    $http({method:'GET', url:'http://localhost:7001/memory/api/v1/codes'})
+        .success(function (data, status, headers, config) {
+            vm.codesList = data;
+        })
+        .error(function (data, status, headers, config) {
+            vm.codesList = Data.getCodes();
+        });
 
 }
 
