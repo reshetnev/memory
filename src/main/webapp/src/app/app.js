ba5189471730b80c41b38c930f450396070fb62c
Aleksandr Reshetnev <a.v.reshetnev@gmail.com>
2015-11-26 16:27:57 +0300

    Added login.html

--- src/main/webapp/src/app/app.js
+++ src/main/webapp/src/app/app.js
@@ -3,13 +3,26 @@
 
     angular
         .module('app', [
-			'app.testData',
+            'ngRoute',
             'app.controllers',
             'app.filters'
-        ]);
+        ])
+        .config(function($routeProvider, $httpProvider) {
+    
+            $routeProvider.when('/', {
+                templateUrl : 'pages/home.html',
+                controller : 'home'
+            }).when('/login', {
+                templateUrl : 'pages/login.html',
+                controller : 'login'
+            }).otherwise('/');
+        
+            $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    
+        })
 
-	angular.module('app.testData', []); // set testData
     angular.module('app.controllers', []); // set ctrls
     angular.module('app.filters', []); // set filters
 
+    
 })();
